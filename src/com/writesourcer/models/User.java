056431f4c6272503cff1223f2900cd2618679514
Christopher DiStasio <cadistasio@gmail.com>
2021-04-22 11:23:24 -0500

    Debugs, adds getter, setter for user profile to User

--- src/com/writesourcer/models/User.java
+++ src/com/writesourcer/models/User.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User {
 
 	@Id
@@ -35,7 +35,7 @@ public class User {
 	private String password;
 	
 	@OneToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name="user_profile")
+	@JoinColumn(name="user_profile_id")
 	private UserProfile userProfile;
 	
 	
@@ -52,92 +52,62 @@ public class User {
 		this.password = password;
 	}
 
-
-
-
 	public int getId() {
 		return id;
 	}
-
-
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
-
 	public String getUsername() {
 		return username;
 	}
-
-
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
-
-
 	public String getFirstName() {
 		return firstName;
 	}
-
-
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
-
-
 	public String getLastName() {
 		return lastName;
 	}
-
-
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	
+	public UserProfile getUserProfile() {
+		return userProfile;
+	}
 
-
+	public void setUserProfile(UserProfile userProfile) {
+		this.userProfile = userProfile;
+	}
 
 	@Override
 	public String toString() {
